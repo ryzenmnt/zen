644f1cc159fecd693d11a480c774e0e7ee84d349
ryzenmnt <160362978+ryzenmnt@users.noreply.github.com>
2025-08-26 22:55:59 +0800

    Add files via upload

---
+++ exercisenum1.py
@@ -0,0 +1,10 @@
+# ExcerciseNumOne
+
+name = input("What is your name?: ")
+age = int(input("How old are you?: "))
+
+age = age + 1
+
+print(f"Hello {name}!")
+print("HAPPY BIRTHDAY!")
+print(f"You are {age} years old")
